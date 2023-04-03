971a87d637ce7123d1c7abccf48b8355a2f7a850
Ksenia <kseniapopova02@gmail.com>
2023-04-03 20:17:07 +0200

    remove: typeProps from Btn

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StyledBtn } from "./styled";
 
-const Button = ({ children, onClick, className, type, disabled }) => {
+const Button = ({
+  children,
+  onClick,
+  className = "",
+  type = "button",
+  disabled = "false",
+}) => {
   const handleClick = (e) => {
     if (!disabled) {
       onClick(e);
@@ -21,18 +27,3 @@ const Button = ({ children, onClick, className, type, disabled }) => {
 };
 
 export default Button;
-
-Button.defaultProps = {
-  onClick: () => {},
-  className: "",
-  type: "button",
-  disabled: false,
-};
-
-Button.propTypes = {
-  onClick: PropTypes.func,
-  className: PropTypes.string,
-  type: PropTypes.string,
-  disabled: PropTypes.bool,
-  children: PropTypes.node.isRequired,
-};
