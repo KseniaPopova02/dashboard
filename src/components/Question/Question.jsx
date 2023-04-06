981276477d633ba5e9fbab01b0eb7f5d6b6f4c03
Ksenia <kseniapopova02@gmail.com>
2023-04-06 10:19:33 +0200

    add: question component

---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledQuestionSpan, StyledQuestionText } from "./style";
+
+export const Question = () => (
+  <StyledQuestionText>
+    Don’t have an account?<StyledQuestionSpan>Sign up</StyledQuestionSpan>
+  </StyledQuestionText>
+);
---
+++ src/components/Question/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledQuestionText = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  color: #9fa2b4;
+`;
+
+export const StyledQuestionSpan = styled.span`
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  color: #3751ff;
+`;
