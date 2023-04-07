a9e46986ae9cee9d2824cdba40040de103bdfd5a
Ksenia <kseniapopova02@gmail.com>
2023-04-07 16:07:59 +0200

    change: name of Input component

--- src/components/InputRegular/InputRegular.jsx
+++ src/components/InputRegular/InputRegular.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { StyledInput } from "./style";
 
-export const Input = ({ type = "text", placeholder = "", className = "" }) => (
+export const InputRegular = ({
+  type = "text",
+  placeholder = "",
+  className = "",
+}) => (
   <StyledInput type={type} placeholder={placeholder} className={className} />
 );
